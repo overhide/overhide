7badcdcf117968080f08435020b4fe4df38e68c7
Jakub Ner <jakub_ner@hotmail.com>
2018-05-06 20:02:28 -0400

    Go with HTML rendered APIs instead of markdown

---
+++ docs/client.js
@@ -0,0 +1,22 @@
+/**
+ * Root namespace for OverHide code.
+ *
+ * @namespace overhide
+ */
+
+
+/**
+ * Utility methods.
+ *
+ * @memberof overhide
+ * @namespace utils
+ */
+
+ /**
+  * Generates mnemonic, returns it, bip39.
+  *
+  * @memberof overhide.utils
+  * @function generateMnemonic
+  * @param {string} passphrase
+  * @returns {string} mnemonic
+  */
